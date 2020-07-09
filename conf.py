8cb414d329e7ab3039bae17ec69e36cc6779d7f8
David Benqué <mail@davidbenque.com>
2020-07-09 17:15:04 +0100

    Install markdown tables package

--- conf.py
+++ conf.py
@@ -30,7 +30,10 @@ release = '3.19.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+extensions = [
+    'recommonmark',
+    'sphinx_markdown_tables'
+]
 
 source_suffix = {
     '.rst': 'restructuredtext',
