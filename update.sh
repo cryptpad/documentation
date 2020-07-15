aa98ff9e7074e0cb8c6f2243cb1dafd0b9ee1dad
David Benqué <mail@davidbenque.com>
2020-07-15 17:04:23 +0100

    Test change on .rst syntax

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@ make gettext
 cat LANG | while  read ligne ; do
     sphinx-intl update --pot-dir _build/gettext -l $ligne
 done
-git add locale/*
-git commit -m "Update .po files"
-git push origin main
+# git add locale/*
+# git commit -m "Update .po files"
+# git push origin main
