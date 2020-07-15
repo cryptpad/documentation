4186139e6dc117b277f1d04aee263410d18c864e
David Benqué <mail@davidbenque.com>
2020-07-15 15:08:48 +0100

    adjust build script

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 git pull origin main
-rm -r www
+rm -rf www
 mkdir www
 mkdir www/en
+rm -rf _build/html
 make html
-rm -r _build/html
 cp -r _build/html www/en/
 cat LANG | while  read ligne ; do
-    rm -r _build/html
+    rm -rf _build/html
     make -e SPHINXOPTS="-D language='$ligne'" html
     mkdir www/$ligne
     cp -r _build/html www/$ligne/
