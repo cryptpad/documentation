f9afc8f0c563116045ffb46daa240867ff95a90e
David Benqué <mail@davidbenque.com>
2020-07-15 15:26:07 +0100

    fix build file

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-git pull origin main
+#git pull origin main
 rm -rf www
 mkdir www
 mkdir www/en
 rm -rf _build/html
 make html
 cp -r _build/html/* www/en/
-cat LANG | while  read ligne ; do
+cat LANG | while read ligne ; do
+    echo $ligne
     rm -rf _build/html
     make -e SPHINXOPTS="-D language='$ligne'" html
     mkdir www/$ligne
