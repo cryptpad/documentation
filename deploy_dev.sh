5a3e60c904fc4562a5b2154346f72acc04066cc4
yflory <yann.flory@xwiki.com>
2020-07-20 16:11:06 +0200

    Fix deploy_dev script

---
+++ deploy_dev.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+git push ssh://cryptpad@freemium.cryptpad.fr:/home/cryptpad/cryptpad-documentation.git dev && ssh cryptpad@freemium.cryptpad.fr 'cd /home/cryptpad/cryptpad-documentation && /usr/bin/git checkout dev && /usr/bin/git pull'
