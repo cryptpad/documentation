169e75a427322e0b7fe84eba830dda5bc65e0af4
David Benqué <mail@davidbenque.com>
2020-07-23 10:52:19 +0100

    Bump version

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ copyright = '2020, CryptPad Team'
 author = 'CryptPad Team'
 
 # The full version, including alpha/beta/rc tags
-release = '3.19.0'
+release = '3.20.1'
 
 
 # -- General configuration ---------------------------------------------------
