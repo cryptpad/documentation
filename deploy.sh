5e00877564379558c8bff40900ed44cdcff48edc
ansuz <ansuz@transitiontech.ca>
2020-07-27 11:11:05 -0400

    update deploy script to target prod

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git push ssh://cryptpad@freemium.cryptpad.fr:/home/cryptpad/cryptpad-documentation.git main && ssh cryptpad@freemium.cryptpad.fr 'cd /home/cryptpad/cryptpad-documentation && /usr/bin/git checkout main && /usr/bin/git pull'
+git push ssh://cryptpad@docs.cryptpad.fr:/home/cryptpad/cryptpad-documentation.git main && ssh cryptpad@docs.cryptpad.fr 'cd /home/cryptpad/cryptpad-documentation && /usr/bin/git checkout main && /usr/bin/git pull'
