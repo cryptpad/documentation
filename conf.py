d3245228f6063b4498ec6778c52b6d170a656b9d
David Benqué <mail@davidbenque.com>
2020-12-17 11:52:22 +0000

    Updates for 3.25

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ copyright = '2020, CryptPad Team'
 author = 'CryptPad Team'
 
 # The full version, including alpha/beta/rc tags
-release = '3.24.0'
+release = '3.25.0'
 
 
 # -- General configuration ---------------------------------------------------
