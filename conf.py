cb37603640f91b7e37205f90ce0547dac2efe8ad
David Benque <david.benque@xwiki.com>
2023-08-10 14:10:11 +0100

    Bump version

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ copyright = '2023, CryptPad Team'
 author = 'CryptPad Team'
 
 # The full version, including alpha/beta/rc tags
-release = '5.3'
+release = '5.4'
 
 
 # -- General configuration ---------------------------------------------------
