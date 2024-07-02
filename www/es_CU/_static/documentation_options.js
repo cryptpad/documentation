0ed5316cad0c58e187ce6b81f447aa5686a6171a
mathilde-cryptpad <156299270+mathilde-cryptpad@users.noreply.github.com>
2024-07-02 12:14:48 +0200

    new build with correct version

--- www/es_CU/_static/documentation_options.js
+++ www/es_CU/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2024.3.0',
+    VERSION: '2024.6.0',
     LANGUAGE: 'es-CU',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
