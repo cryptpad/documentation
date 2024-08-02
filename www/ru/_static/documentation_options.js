604be5bb71dcee294237bfc231a4432ec5167d02
mathilde-cryptpad <156299270+mathilde-cryptpad@users.noreply.github.com>
2024-08-02 15:24:20 +0200

    new build

--- www/ru/_static/documentation_options.js
+++ www/ru/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2024.6.0',
+    VERSION: '2024.6.1',
     LANGUAGE: 'ru',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
